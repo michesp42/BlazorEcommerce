4ed5ea48748874b51cdb3a4e9c9401eadbae1df7
Michael B. Espeña <michael.espena.public@gmail.com>
2023-06-02 09:03:22 +0800

    initial commit
    - first steps in making an ecommerce site using Blazor
    - initial markup for the site

---
+++ Client/Program.cs
@@ -0,0 +1,13 @@
+global using BlazorEcommerce.Shared;
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorEcommerce.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
