42373b5c3ceff7a40c7da628de5b85f10a4c55f9
Michael B. Espeña <michael.espena.public@gmail.com>
2023-06-03 14:21:04 +0800

    add and use the IProductService

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,7 +1,9 @@
-global using BlazorEcommerce.Shared;
-global using BlazorEcommerce.Server.Data;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.AspNetCore.Mvc;
+
+global using BlazorEcommerce.Shared;
+global using BlazorEcommerce.Server.Data;
+global using BlazorEcommerce.Server.Services.ProductService;
 
 using Microsoft.AspNetCore.ResponseCompression;
 
@@ -17,6 +19,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
