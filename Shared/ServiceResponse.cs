6ea8f6ece7455e38885bef5921ee0f149ef77cbe
Michael B. Espeña <michael.espena.public@gmail.com>
2023-06-03 18:19:46 +0800

    Fix variable name

--- Shared/ServiceResponse.cs
+++ Shared/ServiceResponse.cs
@@ -3,6 +3,6 @@ namespace BlazorEcommerce.Shared;
 public class ServiceResponse<T>
 {
     public T? Data { get; set; }
-    public bool Sources { get; set; } = true;
+    public bool Success { get; set; } = true;
     public string Message { get; set; } = string.Empty;
 }
