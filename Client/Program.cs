e7f324a90f8bdffdfed81ec1026dff3ae9d34826
Michael B. Espeña <michael.espena.public@gmail.com>
2023-06-03 15:46:52 +0800

    create product services in the client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,8 +1,12 @@
-global using BlazorEcommerce.Shared;
 global using Microsoft.AspNetCore.Components;
+global using System.Net.Http.Json;
+
+global using BlazorEcommerce.Shared;
+global using BlazorEcommerce.Client.Services.ProductService;
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 using BlazorEcommerce.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,5 +14,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IProductService, ProductService>();
 
 await builder.Build().RunAsync();
