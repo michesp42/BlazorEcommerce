b4fb7323f0a2cc288b05832ab5195bf2e9efc758
Michael B. Espeña <michael.espena.public@gmail.com>
2023-06-03 19:43:11 +0800

    add Category class

--- Shared/Product.cs
+++ Shared/Product.cs
@@ -8,6 +8,8 @@ public class Product
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string ImageUrl { get; set; } = string.Empty;
+    public Category? Category { get; set; }
+    public int CategoryId { get; set; }
 
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
