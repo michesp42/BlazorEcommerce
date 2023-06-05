5b3bf19ac7737b5b96bf4f5550fff21607a2940b
Michael B. Espeña <michael.espena.public@gmail.com>
2023-06-05 19:48:57 +0800

    display price in product list component

--- Client/Shared/ProductList.razor.cs
+++ Client/Shared/ProductList.razor.cs
@@ -17,4 +17,22 @@ public partial class ProductList : IDisposable
     {
         ProductService.ProductsChanged -= StateHasChanged;
     }
+
+    private string GetPriceText(Product product)
+    {
+        var variants = product.Variants;
+
+        if (variants.Count == 0)
+        {
+            return string.Empty;
+        }
+        else if (variants.Count == 1)
+        {
+            return $"${variants[0].Price}";
+        }
+
+        decimal minPrice = variants.Min(v => v.Price);
+
+        return $"Starting at ${minPrice}";
+    }
 }
