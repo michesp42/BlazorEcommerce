de052122101676fe7588b2744aea49a6cf302ea8
Michael B. Espeña <michael.espena.public@gmail.com>
2023-06-10 14:29:45 +0800

    install Blazored.LocalStorage

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,11 +10,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 using BlazorEcommerce.Client;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(
     sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }
 );
