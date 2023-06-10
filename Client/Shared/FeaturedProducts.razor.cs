dd6eda97102c06fe85ed297a11f328efaf7f3337
Michael B. Espeña <michael.espena.public@gmail.com>
2023-06-10 10:51:25 +0800

    show featured products with new component

---
+++ Client/Shared/FeaturedProducts.razor.cs
@@ -0,0 +1,16 @@
+namespace BlazorEcommerce.Client.Shared;
+
+public partial class FeaturedProducts : IDisposable
+{
+    [Inject] IProductService ProductService { get; set; }
+    
+    protected override void OnInitialized()
+    {
+        ProductService.ProductsChanged += StateHasChanged;
+    }
+
+    public void Dispose()
+    {
+        ProductService.ProductsChanged -= StateHasChanged;
+    }
+}
