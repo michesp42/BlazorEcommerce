fa05d043a21cba886ab427d356f562a4f7bf95e9
Michael B. Espeña <michael.espena.public@gmail.com>
2023-06-11 21:28:34 +0800

    implement user registration on the server

--- Server/Services/AuthenticationService/AuthenticationService.cs
+++ Server/Services/AuthenticationService/AuthenticationService.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+
 namespace BlazorEcommerce.Server.Services.AuthenticationService;
 
 public class AuthenticationService : IAuthenticationService
@@ -9,9 +11,22 @@ public class AuthenticationService : IAuthenticationService
         _context = context;
     }
 
-    public Task<ServiceResponse<int>> Register(User user, string password)
+    public async Task<ServiceResponse<int>> Register(User user, string password)
     {
-        throw new NotImplementedException();
+        if (await UserExists(user.Email))
+        {
+            return new ServiceResponse<int> { Success = false, Message = "User already exists." };
+        }
+
+        CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+        user.PasswordHash = passwordHash;
+        user.PasswordSalt = passwordSalt;
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return new ServiceResponse<int> { Data = user.Id };
     }
 
     public async Task<bool> UserExists(string email)
@@ -26,5 +41,18 @@ public class AuthenticationService : IAuthenticationService
         }
 
         return false;
+    }
+
+    private void CreatePasswordHash(
+        string password,
+        out byte[] passwordHash,
+        out byte[] passwordSalt
+    )
+    {
+        using (var hmac = new HMACSHA512())
+        {
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
     }
 }
