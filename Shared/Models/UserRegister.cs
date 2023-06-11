98fab7a3040ac43c16db76543600d621c81aadb5
Michael B. Espeña <michael.espena.public@gmail.com>
2023-06-11 15:09:20 +0800

    add data annotations for validations

--- Shared/Models/UserRegister.cs
+++ Shared/Models/UserRegister.cs
@@ -1,8 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorEcommerce.Shared.Models;
 
 public class UserRegister
 {
+    [Required, EmailAddress]
     public string Email { get; set; } = string.Empty;
+
+    [Required, StringLength(100, MinimumLength = 8)]
     public string Password { get; set; } = string.Empty;
+
+    [Compare("Password", ErrorMessage = "The passwords do not match.")]
     public string PasswordConfirmation { get; set; } = string.Empty;
 }
