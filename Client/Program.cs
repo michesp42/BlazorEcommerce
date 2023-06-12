86f6094c8256ca203d25f06b082cbc71f6c57e42
Michael B. Espeña <michael.espena.public@gmail.com>
2023-06-12 12:30:30 +0800

    expose the authentication state

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,5 @@
 global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Authorization;
 global using System.Net.Http.Json;
 
 global using BlazorEcommerce.Shared.Models;
@@ -13,12 +14,16 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 using BlazorEcommerce.Client;
 using Blazored.LocalStorage;
+using BlazorEcommerce.Client.Misc;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddScoped(
     sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }
 );
