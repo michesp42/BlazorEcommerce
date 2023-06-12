3c37f318afd99c4c82442b2f8366b68e5998d2e7
Michael B. Espeña <michael.espena.public@gmail.com>
2023-06-12 17:12:20 +0800

    add return url to the login

--- Client/Pages/Login.razor.cs
+++ Client/Pages/Login.razor.cs
@@ -1,4 +1,5 @@
 using Blazored.LocalStorage;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace BlazorEcommerce.Client.Pages;
 
@@ -6,23 +7,34 @@ public partial class Login
 {
     [Inject]
     public IAuthenticationService AuthenticationService { get; set; }
-    
+
     [Inject]
     public ILocalStorageService LocalStorageService { get; set; }
-    
+
     [Inject]
     public NavigationManager NavigationManager { get; set; }
-    
+
     [Inject]
     public AuthenticationStateProvider AuthenticationStateProvider { get; set; }
 
     private UserLogin user = new UserLogin();
     private string errorMessage = string.Empty;
+    private string returnUrl = string.Empty;
+
+    protected override void OnInitialized()
+    {
+        var uri = NavigationManager.ToAbsoluteUri(NavigationManager.Uri);
+
+        if (QueryHelpers.ParseQuery(uri.Query).TryGetValue("returnUrl", out var url))
+        {
+            returnUrl = url;
+        }
+    }
 
     private async Task HandleLogin()
     {
         var result = await AuthenticationService.Login(user);
-        
+
         if (result.Success)
         {
             errorMessage = string.Empty;
@@ -30,7 +42,7 @@ public partial class Login
             await LocalStorageService.SetItemAsStringAsync("authenticationToken", result.Data);
             await AuthenticationStateProvider.GetAuthenticationStateAsync();
 
-            NavigationManager.NavigateTo("/");
+            NavigationManager.NavigateTo(returnUrl);
         }
         else
         {
